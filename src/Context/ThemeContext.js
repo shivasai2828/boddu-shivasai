c9279c8200a0ef24a7ff815e696a7e2959ce133b
shivasai <72300535+shivasai2828@users.noreply.github.com>
2024-08-15 14:36:23 +0530

    Update ThemeContext.js

--- src/Context/ThemeContext.js
+++ src/Context/ThemeContext.js
@@ -2,7 +2,7 @@ import React, { useState, createContext } from 'react';
 
 export const ThemeContext = createContext();
 export const ThemeProvider = ({ children }) => {
-    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(true);
 
     const toggleTheme = () => {
         setIsDarkMode(!isDarkMode);
